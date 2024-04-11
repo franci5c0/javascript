95ee0607422d6e1085eba7cf773c97d5e88106d9
franci5c0 <curosenaitecnico@gmail.com>
2024-04-11 08:58:20 -0300

    s

---
+++ condicionais/exercicio.js
@@ -0,0 +1,23 @@
+
+//EXERCÍCIO1
+
+//var vel = 20
+//console.log(`A velocidade do seu carro é: ${vel} km/h`)
+//if(vel>60){
+//console.log('Dirija sempre usando o cinto de segurança')
+//}
+
+//EXERCÍCIO2
+
+//var país = 'EUA'
+//console.log(`vivendo no ${país}`)
+//if(país == 'Brasil'){
+   // console.log('Você é brasileiro')
+//}
+//else { 
+//console.log('Você é estrangeiro!')    
+//}
+
+
+//EXERCÍCIO3
+
