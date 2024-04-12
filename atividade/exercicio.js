ef334511d32b4538b6ac62f0f148235429009dce
franci5c0 <curosenaitecnico@gmail.com>
2024-04-12 10:46:25 -0300

    novooooo

---
+++ atividade/exercicio.js
@@ -0,0 +1,39 @@
+
+//EXERCICIO
+
+//var idade = 18
+//if (idade < 16) {
+   // console.log('não vota')
+//}
+//else if (idade < 18 || idade > 65) {
+  //console.log('voto é opcional')  
+//}
+//else {
+   // console.log('voto é obrigatório')
+//}
+
+
+//EXERCICIO 2
+
+//var agora = new Date()
+
+//var hora = agora.getHours()
+
+//console.log(`agora são exatamente ${hora} horas`)
+
+//if (hora < 12) {
+  //  console.log('bom dia')
+//}
+
+//else if (hora <= 18) {
+ //   console.log('boa tarde')
+//}
+
+//else {
+   // console.log('boa noite')
+//}
+
+
+//EXERCICIO 3
+
+
